12589c769e45e46b67201e376bd8a1d41bc4c5e1
MauroMolino <Mauro.Molino@edu-its.it>
2025-06-23 15:53:32 +0200

    feat: created detail_room
    created detail_room and is first class RoomDetailsWidget

---
+++ app/lib/presentation/widget/others/detail_room.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:app/model/museum/museumActivity/museum_activity_model.dart';
+
+
+class RoomDetailsWidget extends StatelessWidget{
+  const RoomDetailsWidget({
+    super.key
+});
+}
