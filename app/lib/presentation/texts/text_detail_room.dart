7668d8b342587dc30ce9028b500a70d276a07b95
MauroMolino <Mauro.Molino@edu-its.it>
2025-07-07 14:18:28 +0200

    feat: added Json list of rooms, and implemented in detail_room
    made a Json file with a list of text elements and changed detail_room details to work with Json file

--- app/lib/presentation/texts/text_detail_room.dart
+++
@@ -1,6 +0,0 @@
-const textStarWars =
-    "Welcome to a galaxy far, far away!\nThis room is a tribute to the legendary Star Wars universe, the saga created by George Lucas in 1977 that revolutionized science fiction and captured the hearts of generations. Here you'll find a treasure trove of pure passion: vintage collectible action figures, original movie posters, artist illustrations, iconic models, costumes created by Basic Net, and a true gem for fans — the 1:1 scale prop of Han Solo frozen in carbonite, a rare piece by Sideshow.\nA journey through myth, collecting, and nostalgic wonder.";
-
-const assetStarWarsBG = "assets/images/starwarsbackground.png";
-const assetStarWarsMap = "assets/images/starwarsmap.jpg";
-const buttonText = "listen to the text-to-spech";
--- app/lib/presentation/widgets/others/detail_room.dart
+++ app/lib/presentation/widgets/others/detail_room.dart
@@ -1,109 +1,167 @@
+import 'dart:convert';
 import 'package:app/presentation/styles/colors/generic.dart';
-import 'package:app/presentation/texts/text_detail_room.dart';
 import 'package:app/presentation/widgets/bars/home_page_bottom_nav_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-class DetailRoom extends StatelessWidget {
+class DetailRoom extends StatefulWidget {
+  final String name;
+  const DetailRoom({super.key, required this.name});
+
+  @override
+  State<DetailRoom> createState() => _DetailRoomState();
+}
+
+class _DetailRoomState extends State<DetailRoom> {
   static const blueColor = Color.fromARGB(255, 28, 28, 50);
   static const backText = Color(0x9A545165);
   static const lightBlack = Color(0x56000000);
   static const kBlankSpaceWidget = SizedBox(height: 24);
+  static const buttonText = "listen to the text-to-spech";
 
-  final String title;
-  const DetailRoom({super.key, required this.title});
+  List<dynamic> _rooms = [];
+  String _error = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _loadRooms();
+  }
+
+  Future<void> _loadRooms() async {
+    try {
+      final String response = await rootBundle.loadString(
+        'assets/json_detail_room/json_detail_room.json',
+      ); //richiamiamo il Json
+      final data = json.decode(response);
+      setState(() {
+        _rooms = data['rooms'];
+      });
+    } catch (e) {
+      setState(() {
+        _error = 'Errore nel caricamento della stanza: ${e.toString()}';
+      });
+    }
+  }
+
+  Map<String, dynamic>? getRoomByName(String name) {
+    return _rooms.cast<Map<String, dynamic>?>().firstWhere(
+      (room) => room?['name'] == name,
+      orElse: () => null,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
+    final room = getRoomByName(widget.name);
+    if (_error.isNotEmpty) {
+      return Center(child: Text(_error));
+    }
+    if (room == null) {
+      return Center(child: Text('Room not found'));
+    }
+
     return Scaffold(
-      appBar: AppBar(
-        //TODO sliverAppBar
-        title: Text(
-          title,
-          style: TextStyle(
-            color: kPinkColor,
-            fontSize: 28,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 1.5,
-          ),
-        ),
-        centerTitle: true,
-      ),
       body: SizedBox(
         width: double.infinity,
         child: Stack(
           children: [
             Positioned.fill(
-              child: Image.asset(assetStarWarsBG, fit: BoxFit.cover),
-            ), //background image
+              child: Image.asset(room['roomAssetBG'], fit: BoxFit.cover),
+            ),
             Positioned.fill(child: Container(color: lightBlack)),
-            SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Center(
-                    child: Image.asset(assetStarWarsMap, fit: BoxFit.cover),
+            CustomScrollView(
+              slivers: [
+                SliverAppBar(
+                  backgroundColor: blueColor,
+                  pinned: true,
+                  expandedHeight: 120,
+                  flexibleSpace: FlexibleSpaceBar(
+                    centerTitle: true,
+                    title: Text(
+                      room['name'],
+                      style: TextStyle(
+                        color: kPinkColor,
+                        fontSize: 28,
+                        fontWeight: FontWeight.bold,
+                        letterSpacing: 1.5,
+                      ),
+                    ),
                   ),
-                  kBlankSpaceWidget,
-                  Center(
-                    child: Wrap(
-                      children: [
-                        GestureDetector(
-                          onTap: () {},
-                          child: Container(
-                            padding: const EdgeInsets.symmetric(
-                              horizontal: 16,
-                              vertical: 12,
-                            ),
-                            decoration: BoxDecoration(
-                              border: Border.all(
-                                color: kPinkColor, // Border color
-                                width: 1, // Border width
+                ),
+                SliverToBoxAdapter(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Center(
+                        child: Image.asset(
+                          room['roomAssetMap'],
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      kBlankSpaceWidget,
+                      Center(
+                        child: Wrap(
+                          children: [
+                            GestureDetector(
+                              onTap: () {},
+                              child: Container(
+                                padding: const EdgeInsets.symmetric(
+                                  horizontal: 16,
+                                  vertical: 12,
+                                ),
+                                decoration: BoxDecoration(
+                                  border: Border.all(
+                                    color: kPinkColor,
+                                    width: 1,
+                                  ),
+                                  borderRadius: BorderRadius.circular(18),
+                                ),
+                                child: Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    Icon(
+                                      Icons.volume_up_outlined,
+                                      color: kPinkColor,
+                                      size: 30,
+                                    ),
+                                    SizedBox(width: 8),
+                                    const Text(
+                                      buttonText,
+                                      style: TextStyle(fontSize: 20),
+                                    ),
+                                  ],
+                                ),
                               ),
-                              borderRadius: BorderRadius.circular(18),
                             ),
-                            child: Row(
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                Icon(
-                                  Icons.volume_up_outlined,
-                                  color: kPinkColor,
-                                  size: 30,
-                                ),
-                                SizedBox(width: 8),
-                                const Text(
-                                  buttonText,
-                                  style: TextStyle(fontSize: 20),
-                                ),
-                              ],
+                          ],
+                        ),
+                      ),
+                      kBlankSpaceWidget,
+                      Padding(
+                        padding: EdgeInsets.all(25),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: backText,
+                            borderRadius: BorderRadius.circular(18),
+                          ),
+                          child: Padding(
+                            padding: EdgeInsets.fromLTRB(5, 10, 5, 10),
+                            child: Text(
+                              room['roomText'],
+                              style: TextStyle(
+                                fontSize: 22,
+                                fontStyle: FontStyle.italic,
+                              ),
+                              textAlign: TextAlign.center,
                             ),
                           ),
                         ),
-                      ],
-                    ),
-                  ),
-                  kBlankSpaceWidget,
-                  Padding(
-                    padding: EdgeInsets.all(25),
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: backText,
-                        borderRadius: BorderRadius.circular(18),
                       ),
-                      child: Padding(
-                        padding: EdgeInsetsGeometry.fromLTRB(5, 10, 5, 10),
-                        child: Text(
-                          textStarWars,
-                          style: TextStyle(
-                            fontSize: 22,
-                            fontStyle: FontStyle.italic,
-                          ),
-                          textAlign: TextAlign.center,
-                        ),
-                      ),
-                    ),
+                    ],
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
           ],
         ),
