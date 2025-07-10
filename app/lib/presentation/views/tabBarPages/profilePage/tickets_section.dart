01b78bea8d332db8a2afff05986754bdd01beff6
Angelo Lentini <angelolentini1409@gmail.com>
2025-07-10 12:04:49 +0200

    feature: ticket container
    use new widget to do ticket

--- app/lib/presentation/views/tabBarPages/profilePage/tickets_section.dart
+++ app/lib/presentation/views/tabBarPages/profilePage/tickets_section.dart
@@ -2,6 +2,125 @@ import 'package:app/presentation/styles/colors/generic.dart';
 import 'package:app/presentation/styles/typography/section.dart';
 import 'package:app/presentation/views/tabBarPages/profilePage/profile_page.dart';
 import 'package:flutter/material.dart';
+import 'package:ticket_material/ticket_material.dart';
+
+/// Reusable ticket widget using TicketMaterial
+class MyTicketWidget extends StatelessWidget {
+  final String eventDate;
+  final String eventTime;
+  final String eventTitle;
+  final String venueAddress;
+
+  const MyTicketWidget({
+    super.key,
+    required this.eventDate,
+    required this.eventTime,
+    required this.eventTitle,
+    required this.venueAddress,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TicketMaterial(
+      height: 150,
+      colorBackground: const Color(
+        0xFFB8B5D1,
+      ), // Colore grigio-lavanda come Ticket.png
+      radiusBorder: 12,
+      leftChild: _buildLeft(),
+      rightChild: _buildRight(),
+    );
+  }
+
+  Widget _buildLeft() {
+    return Container(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          // Top row: Date and Time
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                eventDate,
+                style: const TextStyle(
+                  color: kBlackColor,
+                  fontSize: 14,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              Text(
+                eventTime,
+                style: const TextStyle(
+                  color: kBlackColor,
+                  fontSize: 14,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+            ],
+          ),
+
+          // Center: Event title in uppercase bold
+          Text(
+            eventTitle.toUpperCase(),
+            style: const TextStyle(
+              color: kBlackColor,
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+          ),
+
+          // Bottom: Location with icon
+          Row(
+            children: [
+              Icon(
+                Icons.location_on,
+                size: 16,
+                color: kBlackColor.withValues(alpha: 0.7),
+              ),
+              const SizedBox(width: 4),
+              Expanded(
+                child: Text(
+                  venueAddress,
+                  style: const TextStyle(
+                    color: kBlackColor,
+                    fontSize: 14,
+                    fontWeight: FontWeight.w400,
+                  ),
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildRight() {
+    return Container(
+      padding: const EdgeInsets.all(16),
+      child: Center(
+        child: Container(
+          width: 80,
+          height: 100,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8),
+            image: const DecorationImage(
+              image: AssetImage('assets/images/ticket/barcode-ticket.png'),
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
 
 class TicketsSection extends StatelessWidget {
   const TicketsSection({super.key});
@@ -56,86 +175,12 @@ class TicketsSection extends StatelessWidget {
 
         const SizedBox(height: 16),
 
-        Container(
-          padding: const EdgeInsets.all(16),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(16),
-            boxShadow: [
-              BoxShadow(
-                color: kBlackColor.withValues(alpha: 0.2),
-                blurRadius: 8,
-                offset: const Offset(0, 4),
-              ),
-            ],
-            image: const DecorationImage(
-              image: AssetImage('assets/images/ticket/Ticket.png'),
-              fit: BoxFit.cover,
-            ),
-          ),
-
-          child: Row(
-            children: [
-              // Barcode
-              Transform.translate(
-                offset: const Offset(-8, 0), // Sposta 8 pixel verso sinistra
-                child: Container(
-                  width: 60,
-                  height: 80,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8),
-                    image: const DecorationImage(
-                      image: AssetImage(
-                        'assets/images/ticket/barcode-ticket.png',
-                      ),
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                ),
-              ),
-
-              const SizedBox(width: 16),
-
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    // Date and time on the same line
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          eventDate,
-                          style: TextStyle(color: kBlackColor, fontSize: 14),
-                        ),
-                        Text(
-                          eventTime,
-                          style: TextStyle(color: kBlackColor, fontSize: 12),
-                        ),
-                      ],
-                    ),
-
-                    const SizedBox(height: 4),
-
-                    Text(
-                      eventName,
-                      style: TextStyle(
-                        color: kBlackColor,
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-
-                    const SizedBox(height: 8),
-
-                    Text(
-                      eventLocation,
-                      style: TextStyle(color: kBlackColor, fontSize: 12),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
+        // Using the new MyTicketWidget with TicketMaterial
+        MyTicketWidget(
+          eventDate: eventDate,
+          eventTime: eventTime,
+          eventTitle: eventName,
+          venueAddress: eventLocation,
         ),
 
         const SizedBox(height: 12),
